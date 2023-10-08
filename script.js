9a9a80496d536a8f9eb8775c3ded0e3db96f46c6
Shyby Mallanveliyil <shybyhamza@gmail.com>
2023-10-08 16:54:09 -0500

    deleted unwanted images

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 
-const description = document.getElementsByClassName('description');
 
-document.getElementsByClass('ptitle').addEventListener('mouseover', showdes);
-// ptitle.addEventListener('click', showdes)
-function showdes() {
-    console.log('hi');
-    description.style.display = "block";
-}
+
+const questions = [ "Solar Radiation", "Evaporation", "Currents"];
+const answers = ["solar panel", "water cycle", "cooling"];
+let score = 0;
+ 
+// function startgame() {
+//     console.log(questions[0])
+//     document.getElementById('startbutton').style.display = 'none';
+    // for (i=0; i=2; i++) {
+    //     let option = answers[0];
+    // document.getElementsByClassName('options')[0].innerText = option;
+   
+    // }
+   
+    
+//}
+
+
+//  for (i=0; i=2; i++ ){
+//     let quest = questions[i];
+//     document.getElementsByTagName('h2').innerText = quest;
+
+//  }
+
