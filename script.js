d5137ee8d26fdf8d7794ef0baa845de123d9c998
Shyby Mallanveliyil <shybyhamza@gmail.com>
2023-10-08 23:14:05 -0500

    Game added

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
 
 
 
-const quiz = [ 
-    ["What does Ocean act like while absorbing Solar Radiation?", "Solar Panel", "water cycle", "cooling", "1"],
-    ["What is the process when water molecules are heated they exchange freely with the air?", "Cooling", "Raining", "Evaporation", "3"],
+const quiz = [
+    ["What is referred to as the long-term pattern of weather conditions in any particular place?", "Climate", "Weather", "Temperature", "1"], 
+    ["What does ocean resemble when absorbing solar radiation?", "Solar Panel", "Water cycle", "Cooling", "1"],
+    ["What is the process when water molecules are heated they exchange freely in the air?", "Cooling", "Rain", "Evaporation", "3"],
     ["What act like a conveyor belt, help to counteract the uneven distribution of solar radiation reaching Earth’s surface?", "Condensation", "Ocean Currents", "Water cycle", "2"],
 ];
 
 var score = 0;
+var index = 0;
 var currentQuestion;
-var index = 0
  
-function startgame() {
+function startGame() {
     // let idx = Math.floor(Math.random() * 3);
     // console.log(quiz[idx]);
+    index = 0;
+    score = 0;
+    document.getElementById('correct').firstChild.textContent = 'Correct!'
 
-     let quest = quiz[index];
-     currentQuestion = quest;
-     displayQuestion(quest);    
+    let quest = quiz[index];
+    currentQuestion = quest;
+    displayQuestion(quest);    
+}
+
+function restartGame() {
+
+    document.getElementById('options').style.display = 'block';
+    document.getElementById('restartbutton').style.display = 'none';
+    document.getElementById('submitanswer').style.display = 'block';
+
+
+    document.getElementById('answer1').checked = false;
+    document.getElementById('answer2').checked = false;
+    document.getElementById('answer3').checked = false;
+
+
+    document.getElementById('correct').style.display = 'none';
+    document.getElementById('wrong').style.display = 'none';
+
+    index = 0;
+
+    startGame();
+
 }
 
 function displayQuestion(quest) {
 
+    document.getElementById('correct').style.display = 'none';
+    document.getElementById('wrong').style.display = 'none';
+    document.getElementById('startbutton').style.display = 'none';
+    document.getElementById('nextque').style.display = 'none';
+
+    document.getElementById('question').style.display = 'block';
+    document.getElementById('options').style.display = 'block';
+    document.getElementById('submitanswer').style.display = 'block';
+    
     document.getElementById('question').textContent = quest[0];
     document.getElementById('anslabel1').textContent = quest[1];
     document.getElementById('anslabel2').textContent = quest[2];
@@ -31,20 +65,12 @@ function displayQuestion(quest) {
     document.getElementById('answer2').checked = false;
     document.getElementById('answer3').checked = false;
 
-    document.getElementById('correct').style.display = 'none';
-    document.getElementById('wrong').style.display = 'none';
-    document.getElementById('startbutton').style.display = 'none';
-    document.getElementById('question').style.display = 'block';
-    document.getElementById('options').style.display = 'block';
-    document.getElementById('submitanswer').style.display = 'block';
-    document.getElementById('nextque').style.display = 'none';
-
 
 }
 
 function submitAnswer() {
     if (document.querySelector('input[name="answers"]:checked') == null) {
-        alert("Please select an option");
+        // alert("Please select an option");
         return;
     }
 
@@ -59,17 +85,10 @@ function submitAnswer() {
         index++;
         if (quiz.length === index) {
             document.getElementById('nextque').style.display = 'none';
-
-            document.getElementById('question').style.display = 'none';
-            document.getElementById('anslabel1').style.display = 'none';
-            document.getElementById('anslabel2').style.display = 'none';
-            document.getElementById('anslabel3').style.display = 'none';
-        
-            document.getElementById('answer1').style.display = 'none';
-            document.getElementById('answer2').style.display = 'none';
-            document.getElementById('answer3').style.display = 'none';
+            document.getElementById('options').style.display = 'none';
         
             document.getElementById('correct').firstChild.textContent = 'Congratulations! Quiz completed!';
+            document.getElementById('restartbutton').style.display = 'block';
 
         }
     } else {
